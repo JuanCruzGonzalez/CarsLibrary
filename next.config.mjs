ef575b0beb5aaf514e21fda96a0d4cb39eb10afe
JuanCruzGonzalez <117318676+JuanCruzGonzalez@users.noreply.github.com>
2024-04-30 11:19:04 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /**
-* @type {import('next').NextConfig}
-*/
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
   },
   assetPrefix: './',
-  module.exports = {
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-  }
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
